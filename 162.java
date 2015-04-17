4b260c04fa25eeed0f3b7da0b9fb17b9b58f0932
yupan <impanyu@gmail.com>
2015-04-16 20:00:01 -0500

    5

---
+++ 162.java
@@ -0,0 +1,17 @@
+//Find Peak Element Array 2.162
+public class Solution {
+    public int findPeakElement(List<Integer> nums) {
+        int size=nums.size();
+        if(size==0) return -1;
+        int l=0;
+        int r=size-1;
+        int mid;
+        while(l<r){
+            mid=(l+r)/2;
+            if(nums.get(mid)<nums.get(mid+1)) l=mid+1;
+            else if(mid==0 || nums.get(mid)>nums.get(mid-1)) return mid;
+            else r=mid-1;
+        }
+        return l;
+    }
+} 
