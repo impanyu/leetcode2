258c5f083a0250198422354edcfb77af056901c3
yupan <impanyu@gmail.com>
2015-10-08 03:06:47 -0500

    242

---
+++ 242.java
@@ -0,0 +1,11 @@
+//2.242 Valid Anagram
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+      char[] sChar=s.toCharArray() ;
+      char[] tChar=t.toCharArray();
+      Arrays.sort(sChar);
+      Arrays.sort(tChar);
+      return Arrays.equals(sChar,tChar);
+    }
+}
