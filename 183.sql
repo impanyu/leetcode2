c55bd9118f904d1c55a0de757a56077b6a4aadf2
yupan <impanyu@gmail.com>
2015-10-20 00:46:34 -0500

    183

---
+++ 183.sql
@@ -0,0 +1,3 @@
+//183 Customers Who Never Order
+# Write your MySQL query statement below
+Select name from Customers Left Join Orders on Customers.Id=Orders.CustomerId  where Orders.customerId is null
